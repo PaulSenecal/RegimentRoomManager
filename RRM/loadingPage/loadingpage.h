e87d6453057ac69cfcafe43fbba3b23a9c0439fe
PaulZ <senecalpaul@outlook.fr>
2023-12-05 14:11:54 +0100

    add loadingPage file

---
+++ RRM/loadingPage/loadingpage.h
@@ -0,0 +1,29 @@
+#ifndef LOADINGPAGE_H
+#define LOADINGPAGE_H
+
+#include <QWidget>
+#include <QProgressBar>
+#include <QLabel>
+
+namespace Ui {
+class LoadingPage;
+}
+
+class LoadingPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit LoadingPage(QWidget *parent = nullptr);
+    ~LoadingPage();
+    void setBarValue(int);
+    int getBarValue();
+
+private:
+    Ui::LoadingPage *ui;
+    QProgressBar *test;
+    QLabel *logo;
+    int barValue;
+};
+
+#endif // LOADINGPAGE_H
