aa8588b9de5e36e8218b63cc5cc4014c108eb1c6
Senecal Paul <senecalpaul@outlook.fr>
2024-01-21 22:50:58 +0100

    fix roomManagerPage with new detail, fix also minor bug

--- RRM/main.cpp
+++ RRM/main.cpp
@@ -1,12 +1,10 @@
-#include "mainwindow.h"
-#include "loadingPage/loadingpage.h"
 #include "loginPage/loginpage.h"
+#include "manageRoom/roomManager.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //loginPage w;
     loginPage w;
     w.show();
     return a.exec();
--- build-RRM-Desktop_Qt_6_6_1_MinGW_64_bit-Debug/ui_roomManager.h
+++ build-RRM-Desktop_Qt_6_6_1_MinGW_64_bit-Debug/ui_roomManager.h
@@ -27,18 +27,42 @@ public:
     QLabel *floor4ImageLabel;
     QLabel *floor3ImageLabel;
     QLabel *nameVariableLabel;
+    QGroupBox *roomDetailGroupBox;
+    QLabel *roomNumberLabel;
+    QLabel *roomNumberLabel_2;
+    QLabel *roomNumberLabel_3;
+    QLabel *roomNumberLabel_4;
+    QLabel *label;
+    QGroupBox *problemDetailGroupBox;
+    QGroupBox *statusGroupBox;
+    QLabel *problemLabel;
+    QLabel *notBusyLabel;
+    QLabel *violetLabel;
+    QLabel *label_5;
+    QLabel *redLabel;
+    QLabel *greenLabel;
+    QLabel *completLabel;
+    QLabel *halfBusyLabel;
 
     void setupUi(QWidget *roomManager)
     {
         if (roomManager->objectName().isEmpty())
             roomManager->setObjectName("roomManager");
         roomManager->resize(1920, 1080);
+        roomManager->setStyleSheet(QString::fromUtf8("background-color: #D9D9D9;\n"
+"color:black;"));
         floorVariableLabel = new QLabel(roomManager);
         floorVariableLabel->setObjectName("floorVariableLabel");
-        floorVariableLabel->setGeometry(QRect(70, 160, 81, 71));
+        floorVariableLabel->setGeometry(QRect(50, 160, 121, 71));
+        QFont font;
+        font.setPointSize(12);
+        font.setBold(true);
+        floorVariableLabel->setFont(font);
+        floorVariableLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
         floorGroupBox = new QGroupBox(roomManager);
         floorGroupBox->setObjectName("floorGroupBox");
         floorGroupBox->setGeometry(QRect(190, 110, 371, 531));
+        floorGroupBox->setStyleSheet(QString::fromUtf8("background: transparent;"));
         floor1ImageLabel = new QLabel(floorGroupBox);
         floor1ImageLabel->setObjectName("floor1ImageLabel");
         floor1ImageLabel->setGeometry(QRect(50, 370, 241, 121));
@@ -57,7 +81,111 @@ public:
         floor3ImageLabel->setPixmap(QPixmap(QString::fromUtf8(":/Floors/images/floors/floor.png")));
         nameVariableLabel = new QLabel(roomManager);
         nameVariableLabel->setObjectName("nameVariableLabel");
-        nameVariableLabel->setGeometry(QRect(690, 10, 241, 71));
+        nameVariableLabel->setGeometry(QRect(750, 10, 311, 71));
+        QFont font1;
+        font1.setPointSize(20);
+        font1.setBold(true);
+        nameVariableLabel->setFont(font1);
+        nameVariableLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        roomDetailGroupBox = new QGroupBox(roomManager);
+        roomDetailGroupBox->setObjectName("roomDetailGroupBox");
+        roomDetailGroupBox->setGeometry(QRect(1400, 110, 291, 621));
+        roomDetailGroupBox->setStyleSheet(QString::fromUtf8("border-style: outset;\n"
+"background: transparent;\n"
+"border-width: 2px;\n"
+"border-radius: 28px;\n"
+"border-color: black;"));
+        roomNumberLabel = new QLabel(roomDetailGroupBox);
+        roomNumberLabel->setObjectName("roomNumberLabel");
+        roomNumberLabel->setGeometry(QRect(100, 40, 91, 31));
+        QFont font2;
+        font2.setPointSize(12);
+        roomNumberLabel->setFont(font2);
+        roomNumberLabel->setStyleSheet(QString::fromUtf8("background: transparent;\n"
+"border: transparent;"));
+        roomNumberLabel_2 = new QLabel(roomDetailGroupBox);
+        roomNumberLabel_2->setObjectName("roomNumberLabel_2");
+        roomNumberLabel_2->setGeometry(QRect(30, 70, 241, 61));
+        roomNumberLabel_2->setStyleSheet(QString::fromUtf8("border-style: outset;\n"
+"background: transparent;\n"
+"border-width: 2px;\n"
+"border-radius: 20px;\n"
+"border-color: black;"));
+        roomNumberLabel_3 = new QLabel(roomDetailGroupBox);
+        roomNumberLabel_3->setObjectName("roomNumberLabel_3");
+        roomNumberLabel_3->setGeometry(QRect(30, 160, 231, 41));
+        roomNumberLabel_3->setStyleSheet(QString::fromUtf8("border-style: outset;\n"
+"background: transparent;\n"
+"border-width: 2px;\n"
+"border-radius: 20px;\n"
+"border-color: black;"));
+        roomNumberLabel_4 = new QLabel(roomDetailGroupBox);
+        roomNumberLabel_4->setObjectName("roomNumberLabel_4");
+        roomNumberLabel_4->setGeometry(QRect(30, 230, 231, 41));
+        roomNumberLabel_4->setStyleSheet(QString::fromUtf8("border-style: outset;\n"
+"background: transparent;\n"
+"border-width: 2px;\n"
+"border-radius: 20px;\n"
+"border-color: black;"));
+        label = new QLabel(roomDetailGroupBox);
+        label->setObjectName("label");
+        label->setGeometry(QRect(40, 82, 41, 41));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/Status/images/see.png")));
+        problemDetailGroupBox = new QGroupBox(roomManager);
+        problemDetailGroupBox->setObjectName("problemDetailGroupBox");
+        problemDetailGroupBox->setGeometry(QRect(230, 700, 1131, 221));
+        problemDetailGroupBox->setStyleSheet(QString::fromUtf8("border-style: outset;\n"
+"background: transparent;\n"
+"border-width: 2px;\n"
+"border-radius: 28px;\n"
+"border-color: black;"));
+        statusGroupBox = new QGroupBox(roomManager);
+        statusGroupBox->setObjectName("statusGroupBox");
+        statusGroupBox->setGeometry(QRect(1390, 730, 271, 161));
+        statusGroupBox->setStyleSheet(QString::fromUtf8("background: transparent;\n"
+"border: transparent;"));
+        problemLabel = new QLabel(statusGroupBox);
+        problemLabel->setObjectName("problemLabel");
+        problemLabel->setGeometry(QRect(60, 130, 201, 21));
+        QFont font3;
+        font3.setPointSize(15);
+        problemLabel->setFont(font3);
+        problemLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        notBusyLabel = new QLabel(statusGroupBox);
+        notBusyLabel->setObjectName("notBusyLabel");
+        notBusyLabel->setGeometry(QRect(60, 10, 121, 31));
+        notBusyLabel->setFont(font3);
+        notBusyLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        violetLabel = new QLabel(statusGroupBox);
+        violetLabel->setObjectName("violetLabel");
+        violetLabel->setGeometry(QRect(10, 130, 31, 31));
+        violetLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        violetLabel->setPixmap(QPixmap(QString::fromUtf8(":/Status/images/Status/violet.png")));
+        label_5 = new QLabel(statusGroupBox);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(10, 50, 31, 31));
+        label_5->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        label_5->setPixmap(QPixmap(QString::fromUtf8(":/Status/images/Status/orange.png")));
+        redLabel = new QLabel(statusGroupBox);
+        redLabel->setObjectName("redLabel");
+        redLabel->setGeometry(QRect(10, 90, 31, 31));
+        redLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        redLabel->setPixmap(QPixmap(QString::fromUtf8(":/Status/images/Status/red.png")));
+        greenLabel = new QLabel(statusGroupBox);
+        greenLabel->setObjectName("greenLabel");
+        greenLabel->setGeometry(QRect(10, 10, 31, 41));
+        greenLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        greenLabel->setPixmap(QPixmap(QString::fromUtf8(":/Status/images/Status/green.png")));
+        completLabel = new QLabel(statusGroupBox);
+        completLabel->setObjectName("completLabel");
+        completLabel->setGeometry(QRect(60, 90, 81, 31));
+        completLabel->setFont(font3);
+        completLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
+        halfBusyLabel = new QLabel(statusGroupBox);
+        halfBusyLabel->setObjectName("halfBusyLabel");
+        halfBusyLabel->setGeometry(QRect(60, 50, 191, 31));
+        halfBusyLabel->setFont(font3);
+        halfBusyLabel->setStyleSheet(QString::fromUtf8("background: transparent;"));
 
         retranslateUi(roomManager);
 
@@ -74,6 +202,22 @@ public:
         floor4ImageLabel->setText(QString());
         floor3ImageLabel->setText(QString());
         nameVariableLabel->setText(QCoreApplication::translate("roomManager", "Name Building Variable", nullptr));
+        roomDetailGroupBox->setTitle(QString());
+        roomNumberLabel->setText(QCoreApplication::translate("roomManager", "Chambre", nullptr));
+        roomNumberLabel_2->setText(QCoreApplication::translate("roomManager", "Historique Occupant", nullptr));
+        roomNumberLabel_3->setText(QCoreApplication::translate("roomManager", "Ajouter un Occupante", nullptr));
+        roomNumberLabel_4->setText(QCoreApplication::translate("roomManager", "Ajouter un Probleme", nullptr));
+        label->setText(QString());
+        problemDetailGroupBox->setTitle(QString());
+        statusGroupBox->setTitle(QString());
+        problemLabel->setText(QCoreApplication::translate("roomManager", "Non Attribuable", nullptr));
+        notBusyLabel->setText(QCoreApplication::translate("roomManager", "Non Occuper", nullptr));
+        violetLabel->setText(QString());
+        label_5->setText(QString());
+        redLabel->setText(QString());
+        greenLabel->setText(QString());
+        completLabel->setText(QCoreApplication::translate("roomManager", "Complet", nullptr));
+        halfBusyLabel->setText(QCoreApplication::translate("roomManager", "Occuper Partiellement", nullptr));
     } // retranslateUi
 
 };
